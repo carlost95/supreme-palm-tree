be9d08554866496c38ec1f9940abbd2645110be2
carlost95 <carlosanibaltejada@gmail.com>
2020-11-05 23:17:22 -0300

    refactor: Se cambio metodos de guardado, actualizacion y change status

---
+++ src/main/java/com/undec/corralon/DTO/DepartamentoDTO.java
@@ -0,0 +1,41 @@
+package com.undec.corralon.DTO;
+
+public class DepartamentoDTO {
+
+    private Integer id;
+    private String nombre;
+    private String abreviatura;
+    private Boolean estado;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAbreviatura() {
+        return abreviatura;
+    }
+
+    public void setAbreviatura(String abreviatura) {
+        this.abreviatura = abreviatura;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
