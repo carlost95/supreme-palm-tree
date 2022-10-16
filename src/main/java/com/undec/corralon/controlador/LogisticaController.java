1ca0f951c12fff8d8fa2b4f240e253a817109214
carlost95 <carlosanibaltejada@gmail.com>
2022-10-16 11:52:18 -0300

    feat: agrega trazado de ruta

--- src/main/java/com/undec/corralon/controlador/LogisticaController.java
+++ src/main/java/com/undec/corralon/controlador/LogisticaController.java
@@ -2,12 +2,16 @@ package com.undec.corralon.controlador;
 
 
 import com.undec.corralon.DTO.Distancia;
+import com.undec.corralon.DTO.Parada;
+import com.undec.corralon.modelo.Remito;
 import com.undec.corralon.modelo.Ruta;
 import com.undec.corralon.service.LogisticaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin("*")
 @RestController
@@ -20,5 +24,10 @@ public class LogisticaController {
     @PostMapping(value = "")
     public ResponseEntity<Ruta> obtenerRuta(@RequestBody Distancia distancias) throws Exception {
         return ResponseEntity.status(HttpStatus.OK).body(logisticaService.obtenerRuta(distancias));
+    }
+
+    @GetMapping(value = "")
+    public ResponseEntity<List<Parada>> obtenerParadas() throws Exception {
+        return ResponseEntity.status(HttpStatus.OK).body(logisticaService.obtenerParadas());
     }
 }
